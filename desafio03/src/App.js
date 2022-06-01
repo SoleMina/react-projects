0ba0d8cee2495cb9a06d6bf6fc49563e796cffa2
SoleMina <karina.pradogutierrez@gmail.com>
2022-05-31 21:56:30 -0500

    update

--- desafio03/src/App.js
+++ desafio03/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from "./components/NavBar";
 import { Cuerpo } from "./components/Cuerpo";
 import { ItemListContainer } from "./components/ItemListContainer";
+
+//bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
