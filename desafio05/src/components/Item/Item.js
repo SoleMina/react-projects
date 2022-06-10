dc3e97af60417ebf344a029a678688458338c5d2
SoleMina <karina.pradogutierrez@gmail.com>
2022-06-10 00:37:13 -0500

    update

--- desafio05/src/components/Item/Item.js
+++ desafio05/src/components/Item/Item.js
@@ -4,6 +4,7 @@ const Item = ({ producto }) => {
   return (
     <>
       <h2>{producto.name}</h2>
+      <img src={producto.pictureUrl} width="150" height="150" />
       <p>Description: {producto.description}</p>
       <p>Stock: {producto.stock}</p>
     </>
--- desafio05/src/helpers/getFetch.js
+++ desafio05/src/helpers/getFetch.js
@@ -1,36 +1,40 @@
 const arrayApi = [
   {
     id: 1,
-    name: "Laptop",
+    title: "Laptop",
     price: 3500,
     stock: 5,
     category: "electro",
-    description: "High quality"
+    description: "High quality",
+    pictureUrl:
+      "https://d598hd2wips7r.cloudfront.net/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/3/1/310H7LA-1_T1613774549.png"
   },
   {
     id: 2,
-    name: "Ipad",
+    title: "Ipad",
     price: 3100,
     stock: 10,
     category: "electro",
-    description: "High quality"
+    description: "High quality",
+    pictureUrl:
+      "https://home.ripley.com.pe/Attachment/WOP_5/2004258794320/2004258794320_2.jpg"
   },
   {
     id: 3,
-    name: "Cellphone",
+    title: "Cellphone",
     price: 1300,
     stock: 20,
     category: "electro",
-    description: "High quality"
+    description: "High quality",
+    pictureUrl:
+      "https://www.edgars.co.za/media/catalog/product/cache/6f59c662b2daff2e8949d8b966c6178b/y/0/y01_-_black.jpg"
   }
 ];
 
 export const getFetch = () => {
-  //acciones
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(arrayApi);
-    }, 3000);
-    //reject("test")
+    }, 2000);
   });
 };
