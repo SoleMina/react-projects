fd2e0def67c5e1d45cae6473910de7f572e5f0c7
SoleMina <karina.pradogutierrez@gmail.com>
2022-07-03 12:55:49 -0500

    update

---
+++ desafio06/src/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCElQEa_OgmxQ36BfggUCuJZsJJU8ll9H0",
+  authDomain: "productos-9b32a.firebaseapp.com",
+  projectId: "productos-9b32a",
+  storageBucket: "productos-9b32a.appspot.com",
+  messagingSenderId: "213292852009",
+  appId: "1:213292852009:web:6929362c278f29076a4d38"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
